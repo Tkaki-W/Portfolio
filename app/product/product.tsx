8b660c0ac3972da31545b251a267b5d56d71b79c
Tkaki-W <takanoda2005@gmail.com>
2025-08-14 15:26:58 +0900

    change something ?

--- app/product/product.tsx
+++ app/product/product.tsx
@@ -8,7 +8,8 @@ export function Product(){
         {img:"/picture/mario.png", title:"スーパーマリオサイト", body:"高校時代に作ったサイト。HTML, CSS, jQueryを組み合わせて制作。", url:"/webcontents_mario/WebContents/html/index.html"},
         {img:"/picture/iwata.png", title:"研究室pythonコード", body:"研究室の課題で作ったもの。様々な議題に対して制作した。", url:"https://github.com/Tkaki-W/iwata_lab_assignment/tree/master"},
         {img:"/picture/monkey.jpg", title:"モンキーハンティング", body:"モンキーハンティングという物理実験をUnity上で再現してコロナ禍の際に部員に発表。", url:"https://m.youtube.com/watch?v=mnZrvFxNT1I&feature=youtu.be"},
-        {img:"/picture/robokon.jpg", title:"ロボットコンテスト", body:"ロボコンサークルにおいて部内大会で作ったC++のコード", url:"https://github.com/Tkaki-W/Taikousen_Program"}
+        {img:"/picture/robokon.jpg", title:"ロボットコンテスト", body:"ロボコンサークルにおいて部内大会で作ったC++のコード", url:"https://github.com/Tkaki-W/Taikousen_Program"},
+        {img:"/picture/portfolio.jpg", title:"ポートフォリオ", body:"本ポートフォリオ、reactを用いて制作した。",url:""}
     ]
     return(
         <div>
